31e52766cdecee0e4eaa3e949ee3d4a756543e60
InnocentEdem <innocent.kuwornu@amalitech.org>
2024-10-15 08:11:43 +0000

    hotfix: update bacckend url

--- src/appConfig/config.ts
+++ src/appConfig/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  backendUrl:  'https://go-auth-v1.onrender.com',
+  backendUrl:  'https://go-auth-backend.amalitech-dev.net',
+  // backendUrl:  'https://go-auth-v1.onrender.com',
   // backendUrl:  'http://localhost:4000',
 };
 
